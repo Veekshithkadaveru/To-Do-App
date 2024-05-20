8878f73f7fe9093707ba7d5a2e52234e0ceee68a
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-05-20 16:59:22 -0400

    Setup Dagger Hilt

--- app/src/main/java/com/example/to_do_app/MainActivity.kt
+++ app/src/main/java/com/example/to_do_app/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.to_do_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
