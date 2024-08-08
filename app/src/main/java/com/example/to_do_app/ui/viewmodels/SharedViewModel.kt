6125f86bc6b5af72dcc17b340a3a9ef98ea6fb8b
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-08 23:12:03 +0530

    Limit title length

--- app/src/main/java/com/example/to_do_app/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/example/to_do_app/ui/viewmodels/SharedViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.to_do_app.data.models.Priority
 import com.example.to_do_app.data.models.ToDoTask
 import com.example.to_do_app.data.repositories.ToDoRepository
+import com.example.to_do_app.util.Constants.MAX_TITLE_LENGTH
 import com.example.to_do_app.util.RequestState
 import com.example.to_do_app.util.SearchAppBarState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -68,6 +69,12 @@ class SharedViewModel @Inject constructor(
             title.value = ""
             description.value = ""
             priority.value = Priority.Low
+        }
+    }
+
+    fun updateTitle(newTitle: String) {
+        if (newTitle.length < MAX_TITLE_LENGTH) {
+            title.value = newTitle
         }
     }
 }
--- app/src/main/java/com/example/to_do_app/util/Constants.kt
+++ app/src/main/java/com/example/to_do_app/util/Constants.kt
@@ -3,9 +3,11 @@ package com.example.to_do_app.util
 object Constants {
     const val DATABASE_TABLE = "todo_table"
     const val DATABASE_NAME = "todo_name"
-    const val LIST_SCREEN="list/{action}"
-    const val TASK_SCREEN="task/{taskId}"
+    const val LIST_SCREEN = "list/{action}"
+    const val TASK_SCREEN = "task/{taskId}"
 
-    const val LIST_ARGUMENT_KEY="action"
-    const val TASK_ARGUMENT_KEY="taskId"
+    const val LIST_ARGUMENT_KEY = "action"
+    const val TASK_ARGUMENT_KEY = "taskId"
+
+    const val MAX_TITLE_LENGTH = 20
 }
