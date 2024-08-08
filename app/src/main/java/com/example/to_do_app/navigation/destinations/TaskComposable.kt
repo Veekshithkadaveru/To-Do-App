af5825259911182910e2c325e731ac3305cf4f5a
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-08 18:21:42 +0530

    Create Priority Drop Down

--- app/src/main/java/com/example/to_do_app/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/example/to_do_app/navigation/destinations/TaskComposable.kt
@@ -19,7 +19,7 @@ fun NavGraphBuilder.taskComposable(
 ) {
     composable(
         route = TASK_SCREEN,
-        arguments = listOf(navArgument(Constants.TASK_ARGUMENT_KEY) {
+        arguments = listOf(navArgument(TASK_ARGUMENT_KEY) {
             type = NavType.IntType
         })
     ) { navBackStackEntry ->
