7482cad32834504bd1bdf261ff1d57f48b2d4663
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-09 18:06:49 +0530

    Create Field Validataions

--- app/src/main/java/com/example/to_do_app/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/to_do_app/ui/screens/task/TaskScreen.kt
@@ -1,12 +1,15 @@
 package com.example.to_do_app.ui.screens.task
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.example.to_do_app.data.models.Priority
 import com.example.to_do_app.data.models.ToDoTask
 import com.example.to_do_app.ui.viewmodels.SharedViewModel
@@ -22,11 +25,23 @@ fun TaskScreen(
     val title: String by sharedViewModel.title
     val description: String by sharedViewModel.description
     val priority: Priority by sharedViewModel.priority
+
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             TaskAppBar(
                 selectedTask = selectedTask,
-                navigateToListScreen = navigateToListScreen
+                navigateToListScreen = { action ->
+                    if (action == Action.NO_ACTION) {
+                        navigateToListScreen(action)
+                    } else {
+                        if (sharedViewModel.validateFields()) {
+                            navigateToListScreen(action)
+                        } else {
+                            displayToast(context = context)
+                        }
+                    }
+                }
             )
         }, content = { paddingValues ->
             TaskContent(
@@ -46,4 +61,12 @@ fun TaskScreen(
             )
         }
     )
+}
+
+fun displayToast(context: Context) {
+    Toast.makeText(
+        context,
+        "Fields empty",
+        Toast.LENGTH_SHORT
+    ).show()
 }
