b72ce971b70a41845bacba91667a0196bbb0fb06
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-08-09 22:47:31 +0530

    Perform CRUD operations- Update Task

--- app/src/main/java/com/example/to_do_app/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/example/to_do_app/ui/viewmodels/SharedViewModel.kt
@@ -86,6 +86,18 @@ class SharedViewModel @Inject constructor(
         }
     }
 
+    private fun updateTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                id = id.value,
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+            repository.updateTask(toDoTask = toDoTask)
+        }
+    }
+
     fun handleDatabaseActions(action: Action) {
         when (action) {
             Action.ADD -> {
@@ -93,7 +105,7 @@ class SharedViewModel @Inject constructor(
             }
 
             Action.UPDATE -> {
-
+                updateTask()
             }
 
             Action.DELETE -> {
